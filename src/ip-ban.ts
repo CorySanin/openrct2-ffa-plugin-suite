5d132802a5ff9d8a8ce685fe6079fc6d7588ddeb
Cory Sanin <corysanin@outlook.com>
2022-02-03 02:38:18 -0600

    chat commands for ip ban plugin

--- src/ip-ban.ts
+++ src/ip-ban.ts
@@ -9,6 +9,7 @@
     const DEFAULT_TIMEOUT = 60;
 
     const PREFIX = new RegExp('^(!|/)');
+    const CMDBAN = new RegExp('^ban ', 'i');
     const CMDUNBAN = new RegExp('^unban($| )', 'i');
 
     let timeout: number, bannedIPs: object, bannedHashes: object, banGroup = -1;
@@ -18,7 +19,7 @@
         if (network.mode === 'server') {
             context.subscribe('action.execute', (e) => {
                 if (e.type === 63) {
-                    banPlayersWithGroup();
+                    banPlayersWithGroup(e);
                 }
                 else if (banGroup === -1 && e.type === 64) {
                     banGroup = network.groups.length - 1;
@@ -56,9 +57,30 @@
                 if (command !== false) {
                     if ((args = doesCommandMatch(command, [CMDUNBAN])) !== false) {
                         if (isPlayerAdmin(getPlayer(e.player))) {
-                            bannedIPs = {};
-                            bannedHashes = {};
-                            outmsg = '{YELLOW}All bans have been undone!';
+                            if (args) {
+                                args = args.split(' ')[0];
+                                delete bannedIPs[args];
+                                outmsg = `{YELLOW}Unbanned ${args}!`;
+                            }
+                            else {
+                                bannedIPs = {};
+                                bannedHashes = {};
+                                outmsg = '{YELLOW}All bans have been undone!';
+                            }
+                        }
+                    }
+                    else if ((args = doesCommandMatch(command, [CMDBAN])) !== false) {
+                        if (isPlayerAdmin(getPlayer(e.player)) && (args as string).length > 0) {
+                            let t: number;
+                            args = args.split(' ');
+                            if ((args as string[]).length > 1) {
+                                t = parseFloat(args[1]);
+                            }
+                            if (!t) {
+                                t = timeout;
+                            }
+                            banIP(args[0], t);
+                            outmsg = `{YELLOW}Banned ${args[0]} for ${t} minutes!`;
                         }
                     }
                     if (outmsg) {
@@ -120,29 +142,40 @@
         }
     }
 
-    function banPlayersWithGroup() {
+    function banPlayersWithGroup(e: GameActionEventArgs) {
         network.players.forEach(player => {
             if (player.group === banGroup) {
                 player.group = network.defaultGroup;
                 banPlayer(player);
+                sendToAdmins(`{YELLOW}Banned ${player.ipAddress} for ${timeout} minutes!`);
             }
         });
     }
 
-    function banPlayer(player: Player) {
+    function banPlayer(player: Player, time?: number) {
+        if (!time) {
+            time = timeout;
+        }
         let ip = player.ipAddress;
         let hash = player.publicKeyHash;
-        bannedIPs[ip] = date.ticksElapsed + (timeout * TICKS_PER_MINUTE);
         bannedHashes[hash] = ip;
-        if (timeout > 0) {
+        banIP(ip, time);
+        network.kickPlayer(getPlayerIndex(player));
+    }
+
+    function banIP(ip: string, time?: number) {
+        if (!time) {
+            time = timeout;
+        }
+        bannedIPs[ip] = date.ticksElapsed + (time * TICKS_PER_MINUTE);
+        if (time > 0) {
             context.setTimeout(() => {
                 if (ip in bannedIPs) {
                     delete bannedIPs[ip];
                 }
                 cleanHashes(false);
-            }, timeout * MS_PER_MINUTE);
+            }, time * MS_PER_MINUTE);
         }
-        network.kickPlayer(getPlayerIndex(player));
     }
 
     function cleanHashes(reduceBlocking = true) {
@@ -187,7 +220,7 @@
 
     registerPlugin({
         name: 'ffa-ip-ban',
-        version: '0.0.4',
+        version: '0.0.5',
         minApiVersion: 17,
         authors: ['Cory Sanin'],
         type: 'remote',
